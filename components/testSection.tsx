574272490ec71fa74069b61c07974a3a1de12bc2
Majdi Athamnih <majdi.athamnih@gmail.com>
2023-11-06 15:16:45 +0300

    implment fetch API

--- components/testSection.tsx
+++ components/testSection.tsx
@@ -2,14 +2,17 @@
 import React, { useState } from "react";
 import CustomButton from "./customButton";
 import DisabledInput from "./disabledInput";
-import { seleniumTest } from "@/test/selenium";
+import { ResultResponse } from "@/entities/testResponse";
 
 const TestSection = () => {
   const [result, setResult] = useState("");
-  const handelStartTest = () => {
-    console.log("Test Started");
-    const testResult = seleniumTest();
-    // setResult(testResult);
+  const handelStartTest = async () => {
+    const testResponse = await fetch("api/test", {
+      method: "GET",
+    });
+    const dataFetch: ResultResponse = await testResponse.json();
+    if (dataFetch.result === "true") setResult("True");
+    else setResult("False");
   };
   return (
     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
@@ -20,7 +23,7 @@ const TestSection = () => {
 
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <CustomButton
-            title="Getting Start"
+            title="Run the test"
             handelClick={handelStartTest}
             customStyle="bg-cyan-500 hover:bg-cyan-600 p-2 w-80 text-white rounded-full"></CustomButton>
           <DisabledInput
